6eabaa72d03fc37898a17b58857266b4fcb40202
Akhmad Khudri <khudri@binadarma.ac.id>
2025-08-08 22:49:11 +0700

    Update form-booking.php

--- views/form-booking.php
+++ views/form-booking.php
@@ -1,21 +1,22 @@
 <form id="stc-booking-form">
-    <label>Nama:</label><br>
+    <label><?php esc_html_e( 'Nama:', 'smarttourismchain' ); ?></label><br>
     <input type="text" id="nama" name="nama" required><br>
 
-    <label>Hotel:</label><br>
+    <label><?php esc_html_e( 'Hotel:', 'smarttourismchain' ); ?></label><br>
     <input type="text" id="hotel" name="hotel" required><br>
 
-    <label>Tanggal:</label><br>
+    <label><?php esc_html_e( 'Tanggal:', 'smarttourismchain' ); ?></label><br>
     <input type="date" name="tanggal" required><br><br>
 
-    <label>Metode Pembayaran:</label><br>
-    <input type="radio" name="payment" value="offchain" checked> Off-Chain
-    <input type="radio" name="payment" value="onchain"> On-Chain<br><br>
+    <label><?php esc_html_e( 'Metode Pembayaran:', 'smarttourismchain' ); ?></label><br>
+    <input type="radio" name="payment" value="offchain" checked> <?php esc_html_e( 'Off-Chain', 'smarttourismchain' ); ?>
+    <input type="radio" name="payment" value="onchain"> <?php esc_html_e( 'On-Chain', 'smarttourismchain' ); ?><br><br>
 
-    <button type="submit">Pesan Sekarang</button>
+    <button type="submit"><?php esc_html_e( 'Pesan Sekarang', 'smarttourismchain' ); ?></button>
 </form>
+
 <div id="hasilKonfirmasi"></div>
+
 <div id="qrcode" style="margin-top: 20px; text-align: center;">
   <!-- QR Code akan muncul di sini -->
 </div>
-
